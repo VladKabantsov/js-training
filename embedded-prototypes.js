57ce71f68f7b0db497e61bcca31c873b34e16096
Vlad Kabantsov <v.kabantsov@aimc.io>
2019-10-15 15:49:02 +0300

    add F.prototype tasks

--- embedded-prototypes.js
+++ embedded-prototypes.js
@@ -1,1 +1,21 @@
-console.log({});
+// task 1
+// Function.prototype.defer = function (ms) {
+//   setTimeout(this, ms);
+// }
+
+// function f() {
+//   console.log('test');
+// }
+
+// f.defer(2000);
+
+// task 2
+Function.prototype.defer = function (ms) {
+  return (...args) => setTimeout(this, ms, ...args);
+};
+
+function f(a, b) {
+  console.log(a + b);
+}
+
+f.defer(1000)(1, 2);
